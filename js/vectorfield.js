a6daa8c3a2c6126757d2f1810a0fdbe5b381fb76
Sujeong Cho <112532259+sujeongcho@users.noreply.github.com>
2023-01-27 16:19:23 +0900

    Update vectorfield.js

--- js/vectorfield.js
+++ js/vectorfield.js
@@ -1,35 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./main.css">
-    <link rel="shortcut icon" href="./image/LOGO_NO.png">
-    
-    <title> 대구미술관</title>
-</head>
-<body>
-    <div class="container">
-        <div class="contents">
-            <div class="header">
-                <div class="logo"><img src="./image/LOGO_NO.png" alt=""></div>
-                <div class="menu">menu</div>
-            </div>
-            <div class="banner1"></div>
-            <div class="banner2">
-                <div class="exh"></div>
-                <div class="prgrm"></div>
-                <div class="cltn"></div>
-            </div>
-            <div class="news"></div>
-            <div class="calendar"></div>
-            <div class="footer"></div>
-        </div>
+let particles = []
+var pix = 10
+var sampling = 200 //數字越大採樣越細
+let canvas;
+let engine;
+let fontSize = 100;
+let fontColor = 'yellow';
 
-    </div>
-</body>
-<script src="./main.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/p5@1.5.0/lib/p5.min.js"></script>
-<script src="./vectorfield.js"></script>
-</html>
+
+function preload(){
+	engine = loadFont('../fonts/NanumSquareNeo-eHv.ttf')
+}
+
+
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight);
+}
+	
+
+function setup() {
+	canvas = createCanvas(windowWidth, windowHeight);
+	canvas.background(0)
+	textFont(engine);
+	textSize(fontSize);
+	colorMode(HSB)
+	for(var i=0; i<width; i+=pix){	
+		for(var j=0; j<height; j+=pix){
+			particles.push({
+				x:i, y:j, clr: color(noise(i/50, j/50)*380, 80, 90)
+			})
+		}
+	}
+}
+
+function draw() {
+	fill('yellow');
+	text('히토 슈타이얼', 490, 200);
+	text('Too Much World', 330,400);
+	text('2023.01.26', 200, 600);
+	text('- 2023.03.31', 600,690)
+	background(0, 0.01)
+	noStroke()
+
+	for(var i=0;i<particles.length;i++){
+		let p = particles[i]
+		fill(p.clr)
+		ellipse(p.x, p.y, 3)		 
+		
+		p.x += (noise(p.x/sampling, p.y/sampling, 1000)-0.5)*8//noise = 0~1之間
+		p.y += (noise(p.x/sampling, p.y/sampling, 10000)-0.5)*8
+	}
+}
